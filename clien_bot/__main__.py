8d066637d5bb12dc059e48a71af2a217aaa2ae09
blurblah <blurblah@blurblah.net>
2019-01-08 14:19:48 +0900

    Apply production config environment

--- clien_bot/__main__.py
+++ clien_bot/__main__.py
@@ -12,22 +12,23 @@ from flask_env import Environments
 def main():
     clien_bot.log.configure_logger()
     logger = logging.getLogger('main')
-    app = connexion.App(__name__, specification_dir='./swagger/')
-    env = Environments(app.app)
+    connecxion_app = connexion.App(__name__, specification_dir='./swagger/')
+    app = connecxion_app.app
+    env = Environments(app, default_env='DEVELOPMENT')
     env.from_yaml('config.yml')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('swagger.yaml', arguments={'title': 'Clien notification bot'})
+    app.json_encoder = encoder.JSONEncoder
+    connecxion_app.add_api('swagger.yaml', arguments={'title': 'Clien notification bot'})
 
-    mongo_uri = app.app.config['MONGO_URI']
-    bot_token = app.app.config['TELEGRAM_BOT_TOKEN']
-    interval = app.app.config['REPEAT_INTERVAL']
-    offset = app.app.config['INTERVAL_OFFSET']
+    mongo_uri = app.config['MONGO_URI']
+    bot_token = app.config['TELEGRAM_BOT_TOKEN']
+    interval = app.config['REPEAT_INTERVAL']
+    offset = app.config['INTERVAL_OFFSET']
     bot = Bot(bot_token, mongo_uri, interval, offset)
     bot.run()
 
-    app.run(port=8080)
+    connecxion_app.run(port=8080)
 
-    # 종료시 bot, scheduler 종료 (shutdown 하지 않으면 bot에서 hang 걸림)
+    # 종료시 bot, scheduler 종료 (bot을 shutdown 하지 않으면 bot에서 hang 걸림)
     logger.info('Shutdown...')
     bot.shutdown()
     logger.info('Shutdown bot.')
