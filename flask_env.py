7531fe300abd6d84fd649c882d4262f9243d48dd
blurblah <blurblah@blurblah.net>
2019-05-09 13:37:40 +0900

    Fix a deprecation warning for loading yaml

--- flask_env.py
+++ flask_env.py
@@ -46,7 +46,7 @@ class Environments(object):
 
     def from_yaml(self, path):
         with open(path) as f:
-            c = yaml.load(f)
+            c = yaml.load(f, Loader=yaml.FullLoader)
 
         for name in self._possible_names():
             try:
