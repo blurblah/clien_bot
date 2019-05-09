882de6de266bd006e1d9c6f571994584f499fdde
blurblah <blurblah@blurblah.net>
2019-05-09 13:37:08 +0900

    Modify usage lines in README. Modify swagger controller

--- crawler/controllers/notifications_controller.py
+++ crawler/controllers/notifications_controller.py
@@ -6,6 +6,9 @@ from flask import current_app as app
 from crawler.models.failure import Failure  # noqa: E501
 from crawler.models.notification import Notification  # noqa: E501
 from crawler.models.success import Success  # noqa: E501
+from crawler.services.data_service import DataService
+from crawler.services.queue_service import QueueService
+import time
 
 logger = logging.getLogger('controller')
 
@@ -27,19 +30,14 @@ def notifications_post(body):  # noqa: E501
         return Failure('Invalid parameters', 400)
 
     mongo_uri = app.config['MONGO_URI']
-    bot_token = app.config['TELEGRAM_BOT_TOKEN']
-    interval = app.config['REPEAT_INTERVAL']
-    offset = app.config['INTERVAL_OFFSET']
-
-    # TODO: 문제가 있는 코드. 구조 개선이 필요함
-    # bot = Bot(bot_token, mongo_uri, interval, offset)
-    # data_service = DataService(mongo_uri)
-    # for chat_id in data_service.select_all_chat_ids():
-    #     try:
-    #         bot.send_message(chat_id, notification.message)
-    #     except Exception as e:
-    #         logger.error('[{}] Exception. Details: {}'.format(chat_id, str(e)))
-    #     # TODO: Spammer 등록 방지용 임시 코드
-    #     time.sleep(0.1)
+    data_service = DataService(mongo_uri)
+    queue_service = QueueService(
+        app.config['MQ_HOST'], app.config['MQ_PORT'], 'allsell'
+    )
+    for chat_id in data_service.select_all_chat_ids():
+        queue_service.publish({'chat_id': chat_id, 'message': notification.message})
+        # TODO: Spammer 등록 방지용 임시 코드
+        time.sleep(0.1)
+    queue_service.disconnect()
 
     return Success()
